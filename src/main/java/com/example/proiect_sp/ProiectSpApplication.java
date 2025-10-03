35dbbcfdda253567cf7752450ae4e3822027b90e
IonXZapada520 <costi.ticusi@gmail.com>
2025-10-03 10:42:14 +0300

    feat: Adauga fisierele initiale ale proiectului SP

---
+++ src/main/java/com/example/proiect_sp/ProiectSpApplication.java
@@ -0,0 +1,13 @@
+package com.example.proiect_sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectSpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProiectSpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proiect_sp/ProiectSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proiect_sp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectSpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
