12f2049444613b3cf41a6ae1664ff7c6fe94b81a
Coasti Ticusi <costi.ticusi@gmail.com>
2025-10-28 19:50:46 +0200

    Repo Initial

---
+++ src/main/java/ro/uvt/info/designpatternslab2025/LapspTicusiDumitrescuCostiNicolaeApplication.java
@@ -0,0 +1,10 @@
+package ro.uvt.info.designpatternslab2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+gitpublic static void main(String[] args) {
+        SpringApplication.run(LapspTicusiDumitrescuCostiNicolaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/uvt/info/designpatternslab2025/LapspTicusiDumitrescuCostiNicolaeApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.designpatternslab2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapspTicusiDumitrescuCostiNicolaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
